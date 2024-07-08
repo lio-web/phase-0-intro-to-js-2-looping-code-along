f2ff7f4b6fa3a69971b4999ec69fea09cb8a61a2
lionard <leonard.muhati@student.moringaschool.com>
2024-07-08 13:29:49 +0300

    assignment compelete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const messages = writeCards(names, birthday);
+for (const message of messages) {
+  console.log(message);
+}
+function writeCards(names,birthday){
+    const thankYouMessage=[];
+    for (let i=0; i<names.length; i++){
+        const message= `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    thankYouMessage.push(message);
+}
+return thankYouMessage;
+}
+
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--; 
+    }
+  } 
